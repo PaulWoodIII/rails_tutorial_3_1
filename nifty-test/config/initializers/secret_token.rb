474ed628dd67b085339b8bc66bee9f64e57dfb73
Paul Wood <paul@Paul-Woods-iMac.local>
2011-09-10 17:13:06 +0800

    created a new blank rails app called nifty test to test ninety generators

---
+++ nifty-test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NiftyTest::Application.config.secret_token = '3293c5d687031de05ca602fe0764c1ce65670168aef55a5e64c7c2b6731596e6a8f79de046bb53723eaf1116c536e773787af70a68d79556e5d7948fd19094c4'
