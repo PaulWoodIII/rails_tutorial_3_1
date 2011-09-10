a1d047edb2580fe778e7e2152dc69f433e9f4e78
Paul Wood <paul@Paul-Woods-iMac.local>
2011-09-10 19:39:00 +0800

    lots of nifty stuff was tried

--- nifty-test/config/routes.rb
+++ nifty-test/config/routes.rb
@@ -1,4 +1,23 @@
 NiftyTest::Application.routes.draw do
+  
+  resources :surveys
+
+  root :to => "posts#index"
+  
+  match 'user/edit' => 'users#edit', :as => :edit_current_user
+
+  match 'signup' => 'users#new', :as => :signup
+
+  match 'logout' => 'sessions#destroy', :as => :logout
+
+  match 'login' => 'sessions#new', :as => :login
+
+  resources :sessions
+
+  resources :users
+
+  resources :posts
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
